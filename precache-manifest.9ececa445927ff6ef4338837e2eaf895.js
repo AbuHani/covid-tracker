50054f8577f52beb586afac50ba53547634445af
abuhani <>
2020-07-19 09:03:53 +0300

    Updates

---
+++ precache-manifest.9ececa445927ff6ef4338837e2eaf895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0864863905c88bd7c1193f72ca02f6e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8189d53294eb0fb66dc",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "fb4617e915e3368db9d8",
+    "url": "/static/js/2.9f5df2c5.chunk.js"
+  },
+  {
+    "revision": "32aeafb3e2d0a73f27822c8ed8045b88",
+    "url": "/static/js/2.9f5df2c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8189d53294eb0fb66dc",
+    "url": "/static/js/main.142ccc56.chunk.js"
+  },
+  {
+    "revision": "8a73b3fa4209a4f8a0c9",
+    "url": "/static/js/runtime-main.396c5088.js"
+  }
+]);
